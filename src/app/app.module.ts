79eaa7f646f6d65faa3706211dcd0b64b6387c10
VoYen <vothikinyen2k@gmail.com>
2022-10-02 17:35:10 +0700

    create products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavLeftComponent } from './nav-left/nav-left.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ProductComponent } from './product/product.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavLeftComponent,
+    MainContentComponent,
+    ProductComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
